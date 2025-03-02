90a21cceab33854c1f0f5eebcd4b0ac004de8cf0
Aman Raj <113578582+huamanraj@users.noreply.github.com>
2025-03-03 02:14:45 +0530

    readmi update

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,17 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),],
+  server: {
+    headers: {
+      'Content-Type': 'application/javascript',
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        format: 'es',
+      },
+    },
+  },
 })
